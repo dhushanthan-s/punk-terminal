cdba52acb247a3da30afe4d57e5dbc792c9f69be
suganth <81741918+suganth591@users.noreply.github.com>
2024-06-05 12:55:48 +0000

    added keyborad buffer with arrows handling

--- punk/src/keyboard/manager.rs
+++ punk/src/keyboard/manager.rs
@@ -20,7 +20,6 @@ impl Buffer {
     }
 
     fn push(&mut self, ch: char)  {
-        println!("Before - {:?}", self.buffer);
         let mut left_str:String = self.buffer[..self.pointer as usize].to_string();
         let right_str:String = self.buffer[self.pointer as usize ..(self.size) as usize].to_string();
 
@@ -28,25 +27,40 @@ impl Buffer {
         left_str.push_str(right_str.as_str());
         self.buffer =  left_str;
         self.pointer += 1;
-        println!("After - {:?}", self.buffer);
+        self.size += 1;
     }
 
     fn backspace(&mut self)
     {
+        if(self.pointer == 0)
+        {
+            return ();
+        }
         let mut left_str:String = self.buffer[..(self.pointer-1) as usize].to_string();
-        let right_str:String = self.buffer[self.pointer as usize ..(self.size+1) as usize].to_string();
+        let right_str:String = self.buffer[self.pointer as usize ..(self.size) as usize].to_string();
 
         left_str.push_str(right_str.as_str());
         self.buffer =  left_str;
+        if( self.pointer > 0)
+        {
+            self.size -= 1;
+            self.pointer -= 1;
+        }
     }
 
     fn delete(&mut self)
     {
+        if(self.pointer >= self.size)
+        {
+            return ();
+        }
         let mut left_str:String = self.buffer[..self.pointer as usize].to_string();
-        let right_str:String = self.buffer[(self.pointer+1) as usize ..(self.size+1) as usize].to_string();
+        let right_str:String = self.buffer[(self.pointer+1) as usize ..(self.size) as usize].to_string();
 
         left_str.push_str(right_str.as_str());
-        self.buffer =  left_str;  }
+        self.buffer =  left_str;  
+        self.size -= 1;
+    }
 
     pub const fn new() -> Buffer {
         Buffer
@@ -57,21 +71,134 @@ impl Buffer {
         }
     }
 
-    fn pass(&mut self)
+    fn get_buffer(&mut self) -> String
     {
+        return self.buffer.clone();
+    }
+
+    fn pass(&mut self, key: Keys)
+    {
+        println!("Kindly handle {:?}", key);
         ()
     }
 
 }
 
-pub fn map_activity(key: Keys, BUFFER: Buffer)
+static mut BUFFER: Buffer = Buffer::new();
+
+pub fn map_activity(key: Keys)
 {
-    match key   {
-        Keys::Left => BUFFER.handle_left(),
-        Keys::Right => BUFFER.handle_right(),
-        Keys::Backtab => BUFFER.backspace(),
-        Keys::Delete => BUFFER.delete(),
-        Keys::Char('A') => BUFFER.push('A'),
-        _ =>  BUFFER.pass()
+    unsafe
+    {
+        match key   {
+            Keys::Left => BUFFER.handle_left(),
+            Keys::Right => BUFFER.handle_right(),
+            Keys::Backtab => BUFFER.backspace(),
+            Keys::Delete => BUFFER.backspace(),
+            Keys::Char(' ') => BUFFER.push(' '),
+            Keys::Char('!') => BUFFER.push('!'),
+            Keys::Char('"') => BUFFER.push('"'),
+            Keys::Char('#') => BUFFER.push('#'),
+            Keys::Char('$') => BUFFER.push('$'),
+            Keys::Char('%') => BUFFER.push('%'),
+            Keys::Char('&') => BUFFER.push('&'),
+            Keys::Char('\'') => BUFFER.push('\''),
+            Keys::Char('(') => BUFFER.push('('),
+            Keys::Char(')') => BUFFER.push(')'),
+            Keys::Char('*') => BUFFER.push('*'),
+            Keys::Char('+') => BUFFER.push('+'),
+            Keys::Char(',') => BUFFER.push(','),
+            Keys::Char('-') => BUFFER.push('-'),
+            Keys::Char('.') => BUFFER.push('.'),
+            Keys::Char('/') => BUFFER.push('/'),
+            Keys::Char('0') => BUFFER.push('0'),
+            Keys::Char('1') => BUFFER.push('1'),
+            Keys::Char('2') => BUFFER.push('2'),
+            Keys::Char('3') => BUFFER.push('3'),
+            Keys::Char('4') => BUFFER.push('4'),
+            Keys::Char('5') => BUFFER.push('5'),
+            Keys::Char('6') => BUFFER.push('6'),
+            Keys::Char('7') => BUFFER.push('7'),
+            Keys::Char('8') => BUFFER.push('8'),
+            Keys::Char('9') => BUFFER.push('9'),
+            Keys::Char(':') => BUFFER.push(':'),
+            Keys::Char(';') => BUFFER.push(';'),
+            Keys::Char('<') => BUFFER.push('<'),
+            Keys::Char('=') => BUFFER.push('='),
+            Keys::Char('>') => BUFFER.push('>'),
+            Keys::Char('?') => BUFFER.push('?'),
+            Keys::Char('@') => BUFFER.push('@'),
+            Keys::Char('A') => BUFFER.push('A'),
+            Keys::Char('B') => BUFFER.push('B'),
+            Keys::Char('C') => BUFFER.push('C'),
+            Keys::Char('D') => BUFFER.push('D'),
+            Keys::Char('E') => BUFFER.push('E'),
+            Keys::Char('F') => BUFFER.push('F'),
+            Keys::Char('G') => BUFFER.push('G'),
+            Keys::Char('H') => BUFFER.push('H'),
+            Keys::Char('I') => BUFFER.push('I'),
+            Keys::Char('J') => BUFFER.push('J'),
+            Keys::Char('K') => BUFFER.push('K'),
+            Keys::Char('L') => BUFFER.push('L'),
+            Keys::Char('M') => BUFFER.push('M'),
+            Keys::Char('N') => BUFFER.push('N'),
+            Keys::Char('O') => BUFFER.push('O'),
+            Keys::Char('P') => BUFFER.push('P'),
+            Keys::Char('Q') => BUFFER.push('Q'),
+            Keys::Char('R') => BUFFER.push('R'),
+            Keys::Char('S') => BUFFER.push('S'),
+            Keys::Char('T') => BUFFER.push('T'),
+            Keys::Char('U') => BUFFER.push('U'),
+            Keys::Char('V') => BUFFER.push('V'),
+            Keys::Char('W') => BUFFER.push('W'),
+            Keys::Char('X') => BUFFER.push('X'),
+            Keys::Char('Y') => BUFFER.push('Y'),
+            Keys::Char('Z') => BUFFER.push('Z'),
+            Keys::Char('[') => BUFFER.push('['),
+            Keys::Char('\\') => BUFFER.push('\\'),
+            Keys::Char(']') => BUFFER.push(']'),
+            Keys::Char('^') => BUFFER.push('^'),
+            Keys::Char('_') => BUFFER.push('_'),
+            Keys::Char('`') => BUFFER.push('`'),
+            Keys::Char('a') => BUFFER.push('a'),
+            Keys::Char('b') => BUFFER.push('b'),
+            Keys::Char('c') => BUFFER.push('c'),
+            Keys::Char('d') => BUFFER.push('d'),
+            Keys::Char('e') => BUFFER.push('e'),
+            Keys::Char('f') => BUFFER.push('f'),
+            Keys::Char('g') => BUFFER.push('g'),
+            Keys::Char('h') => BUFFER.push('h'),
+            Keys::Char('i') => BUFFER.push('i'),
+            Keys::Char('j') => BUFFER.push('j'),
+            Keys::Char('k') => BUFFER.push('k'),
+            Keys::Char('l') => BUFFER.push('l'),
+            Keys::Char('m') => BUFFER.push('m'),
+            Keys::Char('n') => BUFFER.push('n'),
+            Keys::Char('o') => BUFFER.push('o'),
+            Keys::Char('p') => BUFFER.push('p'),
+            Keys::Char('q') => BUFFER.push('q'),
+            Keys::Char('r') => BUFFER.push('r'),
+            Keys::Char('s') => BUFFER.push('s'),
+            Keys::Char('t') => BUFFER.push('t'),
+            Keys::Char('u') => BUFFER.push('u'),
+            Keys::Char('v') => BUFFER.push('v'),
+            Keys::Char('w') => BUFFER.push('w'),
+            Keys::Char('x') => BUFFER.push('x'),
+            Keys::Char('y') => BUFFER.push('y'),
+            Keys::Char('z') => BUFFER.push('z'),
+            Keys::Char('{') => BUFFER.push('{'),
+            Keys::Char('|') => BUFFER.push('|'),
+            Keys::Char('}') => BUFFER.push('}'),
+            Keys::Char('~') => BUFFER.push('~'),
+            _ =>  BUFFER.pass(key)
+        }
+    }
+}
+
+pub fn get_buffer() -> String
+{
+    unsafe
+    {
+        return BUFFER.get_buffer();
     }
 }
--- punk/src/main.rs
+++ punk/src/main.rs
@@ -1,23 +1,25 @@
 extern crate k_board;
 
 use k_board::keys::Keys;
-use keyboard::manager::Buffer;
 
 mod terminal;
 mod keyboard;
 
 
 fn main() {
-    let mut c = 1;
-    let mut BUFFER: Buffer = Buffer::new();
     loop
     {
         let key:Keys = keyboard::buffer::watch();
-        keyboard::manager::map_activity(key, &BUFFER);
-        c+=1;
-        if(c==10)
+        if(key.eq(&Keys::Null))
         {
+            continue;
+        }
+        if(key.eq(&Keys::Ctrl('c')))
+        {
+            println!("Bye ...");
             break;
         }
+        keyboard::manager::map_activity(key);
+        println!("{:?}",keyboard::manager::get_buffer())
     }
 }
