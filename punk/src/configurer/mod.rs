a08746ebebdcb4485549178c68f360d95fc6f017
Z3r0Tw0-ZT <sdhushanthan052@gmail.com>
2024-06-06 16:57:18 +0530

    User config refactor

--- punk/src/configurer/mod.rs
+++ punk/src/configurer/mod.rs
@@ -5,3 +5,11 @@ pub fn path_of_file(file_name: String) -> String
     base_path.push_str(file_name.as_str());
     return base_path;
 }
+
+pub fn path_to_user_conf(file_name: String) -> String
+{
+    let mut base_path: String = std::env::var("HOME").unwrap();
+    base_path.push_str("/.config/punk/");
+    base_path.push_str(&file_name.as_str());
+    return base_path;
+}
