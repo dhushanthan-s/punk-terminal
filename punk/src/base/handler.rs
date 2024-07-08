ef3895996950db95a3a95dedca9c38156ae23637
suganth <81741918+suganth591@users.noreply.github.com>
2024-07-08 14:45:12 +0000

    Overall refactor

---
+++ punk/src/base/handler.rs
@@ -0,0 +1,56 @@
+#[cfg(unix)]
+pub mod unix_handler {
+    use std::io;
+    use std::os::unix::io::AsRawFd;
+    use std::mem;
+    use libc::{termios, tcgetattr, tcsetattr, c_uint, ECHO, ICANON, TCSANOW};
+
+    pub fn enable_raw_mode() -> io::Result<()> {
+        let mut termios = unsafe { mem::zeroed::<termios>() };
+        if unsafe { tcgetattr(io::stdin().as_raw_fd(), &mut termios) } < 0 {
+            return Err(io::Error::last_os_error());
+        }
+        let original_termios = termios;
+        termios.c_lflag &= !(ICANON | ECHO);
+        termios.c_cc[libc::VMIN] = 1;
+        termios.c_cc[libc::VTIME] = 0;
+        if unsafe { tcsetattr(io::stdin().as_raw_fd(), TCSANOW, &termios) } < 0 {
+            return Err(io::Error::last_os_error());
+        }
+        Ok(())
+    }
+}
+
+#[cfg(windows)]
+pub mod windows_handler {
+    use std::io;
+    use std::os::windows::io::{AsRawHandle, RawHandle};
+    use winapi::um::wincon::{
+        ENABLE_ECHO_INPUT, ENABLE_LINE_INPUT, ENABLE_MOUSE_INPUT, ENABLE_PROCESSED_INPUT,
+        ENABLE_QUICK_EDIT_MODE,
+    };
+    use winapi::um::winnt::HANDLE;
+    use winapi::um::consoleapi::{GetConsoleMode, SetConsoleMode};
+    use winapi::um::processenv::GetStdHandle;
+
+    pub fn enable_raw_mode(handle: HANDLE) -> io::Result<()> {
+        let mut mode: u32 = 0;
+        if unsafe { GetConsoleMode(handle, &mut mode as *mut u32) } == 0 {
+            return Err(io::Error::last_os_error());
+        }
+        let new_mode = mode
+            & !(ENABLE_ECHO_INPUT
+                | ENABLE_LINE_INPUT
+                | ENABLE_MOUSE_INPUT
+                | ENABLE_PROCESSED_INPUT
+                | ENABLE_QUICK_EDIT_MODE);
+        if unsafe { SetConsoleMode(handle, new_mode) } == 0 {
+            return Err(io::Error::last_os_error());
+        }
+        Ok(())
+    }
+
+    pub fn get_stdin_handle() -> HANDLE {
+        unsafe { GetStdHandle(winapi::um::winbase::STD_INPUT_HANDLE) }
+    }
+}
--- punk/src/keyboard/buffer.rs
+++ punk/src/keyboard/buffer.rs
@@ -1,10 +1,15 @@
-use k_board::{keyboard::Keyboard, keys::Keys};
+use Key;
+use enums::key_mapper;
+use std::io;
+use std::io::Read;
 
-pub fn watch() -> Keys {
-    let key:Keys = Keys::Enter;
-    let keyboard: Keyboard = Keyboard::new();
-    for key in keyboard {
-        return key;
+static mut INPUT : [u8 ; 5] = [0; 5];
+pub fn watch() -> Key<'static> {
+    let stdin = io::stdin();
+    unsafe
+    {
+        let input_len = stdin.lock().read(&mut INPUT).expect("Failed to read input");
+        let key = &INPUT[0..input_len];
+        return key_mapper::matcher(key);
     }
-    return key;
 }
