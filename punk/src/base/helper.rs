1b4d139459d8bcb596a34ac2565cf775f6d747c5
Dhushanthan <sdhushanthan052@gmail.com>
2026-01-29 18:47:13 +0530

    Redundant code deprecation

--- punk/src/base/helper.rs
+++ punk/src/base/helper.rs
@@ -1,11 +1,8 @@
 use crate::base::os::*;
 
 pub fn initialize_resource() {
-    #[cfg(target_os = "linux")]
+    #[cfg(any(target_os = "linux", target_os = "macos"))]
     linux_specific_handler();
-
-    #[cfg(target_os = "macos")]
-    macos_specific_handler();
 
     #[cfg(target_os = "windows")]
     windows_specific_handler();
--- punk/src/base/os.rs
+++ punk/src/base/os.rs
@@ -1,12 +1,7 @@
 use crate::base::handler::*;
 
-#[cfg(target_os = "linux")]
+#[cfg(any(target_os = "linux", target_os = "macos"))]
 pub fn linux_specific_handler() {
-    let _ = unix_handler::enable_raw_mode();
-}
-
-#[cfg(target_os = "macos")]
-pub fn macos_specific_handler() {
     let _ = unix_handler::enable_raw_mode();
 }
 
